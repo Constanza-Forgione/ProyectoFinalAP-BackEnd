86001022cebf5b7a4eeea0980da9f6f5781b9eca
Constanza Forgione <constanzaforgione@gmail.com>
2023-02-07 22:09:18 -0300

    failed server

--- src/main/java/com/portfolio/CF/CfApplication.java
+++ src/main/java/com/portfolio/CF/CfApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CfApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CfApplication.class, args);
 	}
-
 }
